ce8cdf7dd751f870f3a6b6323173df1183067670
Jon K <jon.knight01@gmail.com>
2020-09-21 22:23:38 +0000

    added documentation to the existing endpoints, some changes to the actual endpoints which will break existing frontend

--- server.js
+++ server.js
@@ -17,20 +17,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
 app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
-
-
-app.get('/', (req, res, next) => res.render('index'));
-
-app.post('/', (req, res, next) => {
-  const sql = req.body.sql;
-  db.all(sql, (err, results) => {
-    res.render('index', { sql, results: results, error: err });
-  });
-});
 
 // if your here to add json support, just dont bother, use queryString.stringify instead on the app
 // TODO check security implications here, apparently this can be bad
@@ -39,6 +27,32 @@ var corsOptions = {
   optionsSuccessStatus: 200
 };
 
+
+
+/**
+ * Lists all the trades by the authorised user 
+ *
+ * Request: {
+ *   isActive: 1 or 0
+ * }
+ *
+ * Response: {
+ *   ID
+ *   ExchangeAccountID
+ *   TradeSystemID
+ *   pair
+ *   isActive
+ *   direction
+ *   size
+ *   assetName
+ *   reasons
+ *   price
+ *   margin
+ *   date
+ * }
+ *
+ * Response Codes: 200
+ */
 app.get('/trades', cors(corsOptions), (req, res, next) => {
 
   // this makes me feel ill...
@@ -51,36 +65,31 @@ app.get('/trades', cors(corsOptions), (req, res, next) => {
     }
   });
 
-
 });
 
-
-app.get('/activeTrades', cors(corsOptions), (req, res, next) => {
-
-  // this makes me feel ill...
-  db.all('SELECT Trades.*, TradeSystems.name as systemName, ExchangeAccounts.name as exchangeAccountName FROM Trades JOIN TradeSystems ON (TradeSystems.ID = Trades.TradeSystemID) JOIN ExchangeAccounts ON (ExchangeAccounts.ID = Trades.ExchangeAccountID) WHERE Trades.isActive = 1', (err, results) => {
-
-   if(err) {
-      res.send(err);
-    } else {
-      res.send(results);
-    }
-  });
-
-});
-
-app.get('/inactiveTrades', cors(corsOptions), (req, res, next) => {
-  // this makes me feel ill...
-  db.all('SELECT Trades.*, TradeSystems.name as systemName, ExchangeAccounts.name as exchangeAccountName FROM Trades JOIN TradeSystems ON (TradeSystems.ID = Trades.TradeSystemID) JOIN ExchangeAccounts ON (ExchangeAccounts.ID = Trades.ExchangeAccountID) WHERE Trades.isActive = 0', (err, results) => {
-
-    if(err) {
-      res.send(err);
-    } else {
-      res.send(results);
-    }
-  });
-});
-
+/**
+ * Record an open trade on your account
+ *
+ * Request: { 
+ *   ExchangeAccountID
+ *   TradeSystemID
+ *   pair
+ *   isActive
+ *   direction
+ *   size
+ *   assetName
+ *   reasons
+ *   price
+ *   margin
+ *   date
+ * } 
+ *
+ * Response: {
+ *   success: true/false
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.post('/trade', cors(corsOptions), (req, res, next) => {
 
   const sql = 'INSERT INTO Trades (ExchangeAccountID, TradeSystemID, pair, isActive, direction, size, assetName, reasons, price, margin, date) VALUES ($ExchangeAccountID, $TradeSystemID, $pair, $isActive, $direction, $size, $assetName, $reasons, $price, $margin, $date)';
@@ -108,7 +117,30 @@ app.post('/trade', cors(corsOptions), (req, res, next) => {
   });
 });
 
-
+/**
+ * Get the trade actions, you can only fetch actions that belong to you.
+ * Trade actions are recordings of in or out events
+ *
+ * TODO: perhaps name these should be called "account events" and not 
+ * "trade actions" as this is a log of the current accounts actual in/out events
+ * 
+ * Request: { 
+ *   :id: TradeId
+ * }
+ *
+ * Response: { 
+ *   ID
+ *   tradeID
+ *   type
+ *   amount
+ *   price
+ *   isComplete
+ *   reason
+ *   created
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/trade/:id/actions', cors(corsOptions), (req, res, next) => {
   db.all('SELECT * FROM TradeActions WHERE TradeID = $tradeId', {$tradeId: req.params.id}, (err, results) => {
 
@@ -120,6 +152,11 @@ app.get('/trade/:id/actions', cors(corsOptions), (req, res, next) => {
   });
 });
 
+/**
+ * Delete a trade by it's id, you can only delete a trade that belongs to you
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/trade/:id/delete', cors(corsOptions), (req, res, next) => {
   db.all('DELETE FROM Trades WHERE Trades.ID = $tradeId', { $tradeId : req.params.id}, (err, results) => {
 
@@ -132,6 +169,17 @@ app.get('/trade/:id/delete', cors(corsOptions), (req, res, next) => {
 
 });
 
+/**
+ * Update a trade by marking it as complete
+ *
+ * TODO: this should also execute any outstanding action logic too, like to sell what remains
+ *
+ * Request: {
+ *   :id: TradeId
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/trade/:id/complete', cors(corsOptions), (req, res, next) => {
   db.all('UPDATE Trades SET isActive = 0 WHERE Trades.ID = $tradeId', { $tradeId : req.params.id}, (err, results) => {
 
@@ -144,20 +192,66 @@ app.get('/trade/:id/complete', cors(corsOptions), (req, res, next) => {
 
 });
 
-// @TODO make queries from them, also need input variables so the correct account is calculated and date ranges used
+/**
+ * Get the exposure of the exchange account
+ *
+ * @TODO use a query, also need input variables such as accountID and start/end date
+ *
+ * Response: {
+ *   value: 10
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/trade/exposure', cors(corsOptions), (req, res, next) => {
   res.send(10);
 });
 
+/**
+ * Get the profit and loss of the exchange account
+ *
+ * @TODO use a query, also need input variables such as accountID and start/end date
+ *
+ * Response: {
+ *   value: 20
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/trade/pnl', cors(corsOptions), (req, res, next) => {
   res.send(20);
 });
 
+/**
+ * Get the win loss ratio of the exchange account
+ *
+ * @TODO use a query, also need input variables such as accountID and start/end date
+ *
+ * Response: {
+ *   value: 60
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/trade/winratio', cors(corsOptions), (req, res, next) => {
   res.send(60);
 });
 
-
+/**
+ * Get all running gekim instances for the account
+ *
+ * @TODO complete
+ *
+ * Response: {
+ *   id
+ *   name
+ *   status
+ *   jsonLaunchSettings
+ *   link-to-access
+ * }
+ *
+ * Response Codes: 200, 404
+ */
 app.get('/strategies', cors(corsOptions), (req, res, next) => {
   //TODO security authentication file?
   var docker = new Docker({host: 'http://gekim.be180.co.uk', port: 2375});
