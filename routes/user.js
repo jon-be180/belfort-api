a2e7765b9982609af49cee35a320a3473b7df0b1
Jon Knight <jon.knight@gmail.com>
2020-09-25 21:20:43 +0100

    added start of MFA code

--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,23 @@ global.fetch = require('node-fetch');
 const AmazonCognitoIdentity = require('amazon-cognito-identity');
 const config = require('../config/cognito.json');
 
+const speakeasy = require('speakeasy')
+const QR = require('qrcode')
+const util = require('./util')
+const name = 'Belfort'
+
 const poolData = {
     UserPoolId: config.cognito.userPoolId,
     ClientId: config.cognito.clientId
 };
 const userData = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-// this is /account
+// this is all inside /account
 router
   .post('/signup', function(req, res) {
+
+    req.check('email', 'invalid email').isEmail()
+
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
@@ -38,19 +46,16 @@ router
 
   })
   .post('/login', (req, res) => {
-    const loginDetails = {
+
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
         Username: req.body.email,
-        Password: req,body.password
-    }
+        Password: req.body.password
+    })
 
-    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(loginDetails)
-
-    const userDetails = {
+    const cognitoUser = new AmazonCognitoIdentity.CognitoUser({
         Username: req.body.email,
         Pool: userPool
-    }
-
-    const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userDetails)
+    })
 
     cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: data => {
@@ -61,6 +66,82 @@ router
           req.send({status: 'error', reason: err.message})
         }
     })
+
+  })
+  .get('/mfa', (req, res) => {
+    if(!req.session.sub) {
+        return res.send({status: 'err'r})
+    }
+    //todo
+  })
+  .get('/mfa/qr-code', (req, res) => {
+      const secret = speakeasy.generateSecret({name})
+
+      if(!req.session['temp-qr-code-secret']) {
+          req.session['temp-qr-code-secret'] = secret.base32
+      }
+
+      QR.toDataURL(secret.otpauth_url, (err, data) => res.send(err ? err : data))
+  })
+  .post('/mfa', (req, res) => {
+      req.session['mfa-errors'] = []
+      if(req.session.sub && !req.body.password) {
+        req.session['mfa-errors'].push('please provide your password');
+        return req.send({status: 'error'})
+      }
+
+      const code = req.body.code
+      if(!code) {
+          req.session['mfa-errors'].push('please provide your google auth code')
+          return res.send({status: 'error'})
+      }
+
+      const verified = String(speakeasy.totp.verify({
+          secret: req.session['temp-qr-code-secret'],
+          encoding: base32,
+          token: code
+      }))
+
+      req.session['mfa-verified'] = verified
+
+      if(req.sesion.sub) {
+        const username = req.session.sub
+        const password = req.body.password
+        const loginDetails = {username, password}
+
+        const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(loginDetails)
+
+        const userDetails = { Username, Pool: util.userPool}
+        const cognitoUser = new AmazonCognitoIdentity.cognitoUser(userDetails)
+
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: data => {
+                // note, doesnt use cognito mfa but will work for google authenticator
+                const mfaAttribute = {
+                    Name: 'custom:mfa',
+                    Value: verified
+                }
+
+                const attribute = new AmazonCognitoIdentity.CognitoUserAttribute(mfaAttribute)
+
+                cognitoUser.updateAttribute([attrubute], (err, data) => {
+                    if(err) {
+                        req.session['login-errors'].push(err.message)
+                        return res.send({status: 'error'})
+                    }
+                })
+
+                // sign them out to re-auth
+            },
+            onFailure: err => {
+                req.session['login-errors'] = []
+                req.session['login-errors'].push(err.message)
+                return res.send({status: 'error'})
+            }
+        }
+      } else {
+          req.send({status: 'error'})
+      }
 
   })
 
